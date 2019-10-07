6843dde1b31072799c5c9dd9e88c69042e9e0cd0
Ajin Sunny <ajin.sunny@github.com>
2019-10-07 13:09:28 -0400

    variables

---
+++ variables/variables.c
@@ -0,0 +1,27 @@
+/*
+Written by: Ajin Sunny
+Purpose: Variables
+Date: 10-7-2019
+
+*/
+
+
+#include <stdio.h>
+#include <stdbool.h>
+
+
+int main()
+{   
+
+    int num = 23;
+    float decimal = 23.22;
+    double large_decimal = 22.333333333333333333;
+    bool mybool = true;
+    
+
+
+
+
+    return 0;
+
+}
