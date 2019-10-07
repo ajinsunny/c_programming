0bf289f98af855522d910eb0f64579362629379e
Ajin Sunny <ajin.sunny@github.com>
2019-10-07 14:06:31 -0400

    challenge_2

--- challenge_2_enums/challenge_enums_company.c
+++ challenge_2_enums/challenge_enums_company.c
@@ -14,6 +14,17 @@ int main()
 {
 
 
+    enum Company {GOOGLE, FACEBOOK, XEROX, EBAY = 11, YAHOO, MICROSOFT};
+    enum Company xerox = XEROX;
+    enum Company google = GOOGLE;
+    enum Company ebay = EBAY;
+
+    printf("The value of xerox is : %d \n", xerox);
+    printf("The value of google is : %d \n", google);
+    printf("The value of ebay is : %d \n", ebay);
+
+
+
 
     return 0;
 
