9dd9f261e6e0d24b745712e262053d210a4932cf
Ajin Sunny <ajin.sunny@gmail.com>
2019-12-17 11:41:37 -0500

    updated the code with few changes from the author

--- section_07_control_flow/challenge_amount_pay/amount_pay.c
+++ section_07_control_flow/challenge_amount_pay/amount_pay.c
@@ -6,33 +6,40 @@ Date: 11-29-2019
 
 */
 
+
+
 #include<stdio.h>
 #include<stdlib.h>
+
+
+
+//define preprocessor directives 
+#define PAY_RATE 12.00
 #define TAXRATE_300 0.15
 #define TAXRATE_150 0.20
 #define TAXRATE_REST 0.25
+#define OVERTIME 40
 
 int main()
 {
     int hours = 0;
-    double basic_pay_rate = 12.0;
     double gross_pay = 0.0;
     double net_pay = 0.0;
-    double overtime_pay = 0.0;
     double taxes = 0.0;
 
     printf("Enter the number hours worked during the week: ");
+    //getting the input 
     scanf("%d",&hours);
+
+    //calculte gross pay 
     if(hours <= 40)
-    gross_pay = basic_pay_rate*hours;
+        gross_pay = PAY_RATE*hours;
     else
     {
-        hours = hours - 40;
-        overtime_pay = basic_pay_rate*hours*1.5;
+        gross_pay = PAY_RATE*hours;
+        double overTimePay = (hours - 40) * (PAY_RATE * 1.5);
+        gross_pay += overTimePay;
     }
-
-    gross_pay += overtime_pay;
-
 
     //calculate taxes 
 
@@ -51,22 +58,17 @@ int main()
 
     }
     
-
+    // net pay
     net_pay = gross_pay - taxes;
 
     //print everything
 
-    printf("Your gross pay this week is: %.2f\n",gross_pay);
-    printf("Your taxes this week is: %.2f\n",taxes);
-    printf("Your netpay this week is: %.2f\n",net_pay);
+    printf("Your gross pay this week is: %.2f\n", gross_pay);
+    printf("Your taxes this week is: %.2f\n", taxes);
+    printf("Your netpay this week is: %.2f\n", net_pay);
 
     return 0;
 
-
-
-
-
-    return  0;
 
 }
 
